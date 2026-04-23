d793a47b48e6ee8bf307ae5d8d027758c030f8ac
André Luiz Barros <62780694+lbarrosandre@users.noreply.github.com>
2026-04-23 19:51:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from supabase import create_client
 
+app = FastAPI()
+
+SUPABASE_URL = "COLE_SUA_URL"
+SUPABASE_KEY = "COLE_SUA_KEY"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+class Login(BaseModel):
+    email: str
+    senha: str
+
+@app.get("/")
+def home():
+    return {"msg": "Rush API online 🚀"}
+
+@app.post("/login")
+def login(data: Login):
+    res = supabase.table("usuarios").select("*").eq("email", data.email).execute()
+
+    if not res.data:
+        return {"erro": "Usuário não encontrado"}
+
+    user = res.data[0]
+
+    if user["senha"] != data.senha:
+        return {"erro": "Senha inválida"}
+
+    return {"msg": "ok", "user": user}
